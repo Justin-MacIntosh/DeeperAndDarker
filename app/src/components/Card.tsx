40bf6cd97052f531d033c51fafec4cd8c99374f5
Justin MacIntosh <jmacintosh@healthverity.com>
2025-07-28 13:43:46 -0400

    React App up and running with new styles

---
+++ app/src/components/Card.tsx
@@ -0,0 +1,27 @@
+import './Card.css';
+
+interface CardProperties {
+  icon: React.ReactNode,
+  content: React.ReactNode
+  suffix: React.ReactNode,
+  color: 'blue' | 'red' | 'green';
+}
+
+const Card = (props: CardProperties) => {
+  const colorClass = `card-${props.color}`;
+  return (
+    <div className={`card noselect ${colorClass}`}>
+      <div className="card-icon">
+        {props.icon}
+      </div>
+      <div className="card-content">
+        {props.content}
+      </div>
+      <div className="card-suffix">
+        {props.suffix}
+      </div>
+    </div>
+  )
+}
+
+export default Card;
