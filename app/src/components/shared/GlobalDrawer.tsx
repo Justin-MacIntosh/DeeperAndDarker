6bd61f5f045d9909b9fcfb1195a72a2cde376191
Justin MacIntosh <jmacintosh@healthverity.com>
2025-08-07 15:16:18 -0400

    Updated globaldrawer to be dynamic

--- app/src/components/shared/GlobalDrawer.tsx
+++ app/src/components/shared/GlobalDrawer.tsx
@@ -1,49 +1,72 @@
 import { useState } from 'react';
 
 import Drawer from 'react-modern-drawer'
+import { useGameStore } from '../../game_state/GameStore';
+import { useShallow } from 'zustand/react/shallow';
 
 
+// GlobalDrawer component that provides a navigation drawer for stage selection
 const GlobalDrawer = ({ setCurrentStage }: { setCurrentStage: (stageId: string) => void }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const stageIds = useGameStore(
+    useShallow((state) => Object.keys(state.stages))
+  );
 
-  const toggleDrawer = () => {
-      setIsOpen((prevState) => !prevState)
-  }
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
   return (
     <>
       <Drawer
-        open={isOpen}
-        onClose={toggleDrawer}
-        direction='left'
+        open={isDrawerOpen}
+        onClose={() => { setIsDrawerOpen(false); }}
+        direction="left"
         zIndex={1000}
-        size={'200px'}
+        size={"200px"}
         duration={300}
         className="border-right border-gray-300 border-r-solid border-r-[3px]"
       >
         <div className="h-full bg-gray-800 text-gray-300 flex flex-col p-5">
-          <button
-            className="btn-default bg-gray-600 mb-5"
-            onClick={() => setCurrentStage("stage_1")}
-          >
-            Deep space
-          </button>
-          <button
-            className="btn-default bg-gray-600"
-            onClick={() => setCurrentStage("stage_2")}
-          >
-            Planet Yan
-          </button>
+          {stageIds.map((stageId) => (
+            <StageSelectButton
+              key={stageId}
+              stageId={stageId}
+              setCurrentStage={setCurrentStage}
+            />
+          ))}
         </div>
-        <button
-          className="cursor-default fixed top-[120px] left-[200px] z-20 w-2 bg-gray-300 h-20 rounded-r-xl"
-        />
+        <button className="cursor-default fixed top-[120px] left-[200px] z-20 w-2 bg-gray-300 h-20 rounded-r-xl" />
       </Drawer>
       <button
         className="cursor-default fixed top-[120px] z-20 w-2 bg-gray-300 h-20 rounded-r-xl"
-        onMouseOver={toggleDrawer}
+        onMouseOver={() => { setIsDrawerOpen(true); }}
       />
     </>
+  );
+}
+
+// StageSelectButton component that renders a button for navigating to each stage
+const StageSelectButton = (
+  { stageId, setCurrentStage }:
+  { stageId: string; setCurrentStage: (stageId: string) => void }
+) => {
+  const stageName = useGameStore(
+    (state) => state.stages[stageId].name
+  );
+
+  // Check if the stage is active before rendering the button
+  const isStageActive = useGameStore(
+    (state) => state.stages[stageId].isActive
+  );
+  if (!isStageActive) {
+    return null; // Don't render inactive stages
+  }
+
+  return (
+    <button
+      className="btn-default bg-gray-600 mb-5"
+      onClick={() => { setCurrentStage(stageId); }}
+    >
+      {stageName}
+    </button>
   );
 }
 
